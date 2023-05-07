4343590399e7c58b5b4a3e842f883131ac16260e
Roy-Kid <lijichen365@126.com>
2023-05-07 17:56:04 +0800

    add load data sidebar

--- src/components/SecondSidebar.tsx
+++ src/components/SecondSidebar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Box from '@mui/joy/Box';
+import Box, { BoxProps } from '@mui/joy/Box';
 import Chip from '@mui/joy/Chip';
 import List from '@mui/joy/List';
 import ListSubheader from '@mui/joy/ListSubheader';
@@ -11,29 +11,100 @@ import IconButton from '@mui/joy/IconButton';
 import Typography from '@mui/joy/Typography';
 import Sheet from '@mui/joy/Sheet';
 import { closeSidebar } from '../utils/sidebarController';
+import { Button } from '@mui/joy';
+import Input from '@mui/joy/Input';
+import { FiPlus, FiRefreshCcw } from 'react-icons/fi';
+
+function SidePane(props: BoxProps) {
+  return (
+    <Box
+      className="Inbox"
+      {...props}
+      sx={[
+        {
+          py: 1,
+          bgcolor: 'background.surface',
+          borderRight: '1px solid',
+          borderColor: 'divider',
+          display: {
+            xs: 'none',
+            md: 'initial',
+          },
+        },
+        ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
+      ]}
+    />
+  );
+}
+
+const content: Record<string, JSX.Element> = {
+  'file': <div>   
+      <SidePane>
+        <Box
+          sx={{
+            p: 1,
+            pb: 0.1,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Typography
+            fontSize="xs2"
+            textColor="text.tertiary"
+            textTransform="uppercase"
+            letterSpacing="md"
+            fontWeight="lg"
+          >
+            Load Data
+          </Typography>
+          <IconButton size="sm" variant="plain" >
+            <FiPlus />
+          </IconButton>
+          <IconButton size="sm" variant="plain" >
+            <FiRefreshCcw />
+          </IconButton>
+        </Box>
+        <Box sx={{ p: 1 }}>
+          <Input size="sm" disabled placeholder="File name" />
+        </Box>
+        <Box
+          sx={{
+            p: 1,
+            pb: 0.1,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Typography
+            fontSize="xs2"
+            textColor="text.tertiary"
+            textTransform="uppercase"
+            letterSpacing="md"
+            fontWeight="lg"
+          >
+            trajectory
+          </Typography>
+          <IconButton size="sm" variant="plain" >
+            <FiPlus />
+          </IconButton>
+          <IconButton size="sm" variant="plain" >
+            <FiRefreshCcw />
+          </IconButton>
+        </Box>
+        <Box sx={{ p: 1 }}>
+          <Input size="sm" disabled placeholder="File name" />
+        </Box>
+      </SidePane>
+  </div>
+};
+
 
 export default function SecondSidebar() {
+
   return (
     <React.Fragment>
-      {/* <Box
-        className="SecondSidebar-overlay"
-        sx={{
-          position: 'fixed',
-          zIndex: 9998,
-          top: 0,
-          left: 0,
-          width: '100vw',
-          height: '100vh',
-          bgcolor: 'background.body',
-          opacity: 'calc(var(--SideNavigation-slideIn, 0) - 0.2)',
-          transition: 'opacity 0.4s',
-          transform: {
-            xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1) + var(--SideNavigation-slideIn, 0) * var(--FirstSidebar-width, 0px)))',
-            lg: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1) + var(--SideNavigation-slideIn, 0) * var(--FirstSidebar-width, 0px)))',
-          },
-        }}
-        onClick={() => closeSidebar()}
-      /> */}
       <Sheet
         className="SecondSidebar"
         sx={{
@@ -51,95 +122,15 @@ export default function SecondSidebar() {
           zIndex: 9999,
           height: '100dvh',
           top: 0,
-          p: 2,
-          py: 3,
+          p: 1,
+          py: 2,
           flexShrink: 0,
           display: 'flex',
           flexDirection: 'column',
           gap: 2,
         }}
       >
-        <List
-          sx={{
-            '--ListItem-radius': '8px',
-            '--ListItem-minHeight': '32px',
-            '--List-gap': '4px',
-          }}
-        >
-          <ListSubheader role="presentation" sx={{ color: 'text.primary' }}>
-            Dashboard
-          </ListSubheader>
-          <ListItem>
-            <ListItemButton onClick={() => closeSidebar()}>
-              <ListItemDecorator>
-                <i data-feather="activity" />
-              </ListItemDecorator>
-              <ListItemContent>Overview</ListItemContent>
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton onClick={() => closeSidebar()}>
-              <ListItemDecorator>
-                <i data-feather="bell" />
-              </ListItemDecorator>
-              <ListItemContent>Notification</ListItemContent>
-              <Chip variant="soft" size="sm">
-                10
-              </Chip>
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton onClick={() => closeSidebar()}>
-              <ListItemDecorator>
-                <i data-feather="bar-chart" />
-              </ListItemDecorator>
-              <ListItemContent>Analytics</ListItemContent>
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton onClick={() => closeSidebar()}>
-              <ListItemDecorator>
-                <i data-feather="star" />
-              </ListItemDecorator>
-              <ListItemContent>Saved reports</ListItemContent>
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton selected variant="soft">
-              <ListItemDecorator>
-                <i data-feather="shopping-cart" />
-              </ListItemDecorator>
-              <ListItemContent>Orders</ListItemContent>
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton onClick={() => closeSidebar()}>
-              <ListItemDecorator>
-                <i data-feather="user" />
-              </ListItemDecorator>
-              <ListItemContent>User reports</ListItemContent>
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton onClick={() => closeSidebar()}>
-              <ListItemDecorator>
-                <i data-feather="settings" />
-              </ListItemDecorator>
-              <ListItemContent>Manage notifications</ListItemContent>
-            </ListItemButton>
-          </ListItem>
-        </List>
-        <Box sx={{ pl: 1, mt: 'auto', display: 'flex', alignItems: 'center' }}>
-          <div>
-            <Typography fontWeight="lg" level="body2">
-              Olivia Ryhe
-            </Typography>
-            <Typography level="body2">olivia@email.com</Typography>
-          </div>
-          <IconButton variant="plain" sx={{ ml: 'auto' }}>
-            <i data-feather="log-out" />
-          </IconButton>
-        </Box>
+        {content["file"]}
       </Sheet>
     </React.Fragment>
   );
